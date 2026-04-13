26d62a3e96342824222b038c7a92060bf00a501b
KaikeMioranza <kaikemioranza@hotmail.com>
2026-04-13 20:09:54 -0300

    feature/hexagonal

---
+++ src/main/java/com/fag/lucasmartins/arquitetura_software/infrastructure/adapters/in/rest/controller/PessoaControllerAdapter.java
@@ -0,0 +1,27 @@
+package com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.rest.controller;
+
+import com.fag.lucasmartins.arquitetura_software.application.ports.in.service.PessoaServicePort;
+import com.fag.lucasmartins.arquitetura_software.core.domain.bo.PessoaBO;
+import com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.rest.dto.PessoaDTO;
+import com.fag.lucasmartins.arquitetura_software.infrastructure.adapters.in.rest.mapper.PessoaDTOMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaControllerAdapter {
+
+    private final PessoaServicePort pessoaServicePort;
+
+    public PessoaControllerAdapter(PessoaServicePort pessoaServicePort) {
+        this.pessoaServicePort = pessoaServicePort;
+    }
+
+    @PostMapping
+    public ResponseEntity<PessoaDTO> cadastrarPessoa(@RequestBody PessoaDTO pessoaDTO) {
+        PessoaBO bo = PessoaDTOMapper.toBo(pessoaDTO);
+        PessoaBO salvo = pessoaServicePort.salvar(bo);
+        PessoaDTO resposta = PessoaDTOMapper.toDto(salvo);
+        return ResponseEntity.status(201).body(resposta);
+    }
+}
